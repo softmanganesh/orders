712d66c311a0541bb705bb43a9f419c948225518
softmanganesh <softmanganesh@gmail.com>
2026-06-17 16:41:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -210,6 +210,7 @@ async function submitOrder() {
 
         const response = await fetch(APP_SCRIPT_URL, {
             method: 'POST',
+             mode: 'no-cors',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(orderData)
         });
